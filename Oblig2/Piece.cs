c2351daadd7520af3e1df552215a188b891fb213
Torjus Tveten <55440508+Torjustveten@users.noreply.github.com>
2020-02-14 12:26:15 +0100

    Add project files.

---
+++ Oblig2/Piece.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Oblig2
+{
+    
+    abstract class Piece
+        {
+            public string Symbol { get; set; }
+
+            protected Piece()
+            {
+            }
+
+            public abstract bool Move(string fromPosition, string toPosition);
+        }
+}
+
---
+++ Oblig2/Rook.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oblig2
+{
+    class Rook : Piece
+    {
+        public Rook()
+        {
+            base.Symbol = "TRN";
+        }
+
+        public override bool Move(string fromPosition, string toPosition)
+        { 
+            return fromPosition[0] == toPosition[0] || fromPosition[1] == toPosition[1];
+        }
+    }
+}
+    
